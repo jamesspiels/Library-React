bfde689707092d0dceeb4a01ec51e84fcf519430
jamesspiels <jspielman52@gmail.com>
2024-07-16 22:31:17 -0400

    init commit

---
+++ components/Landing.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import UndrawBooks from '../assets/Undraw_Books.svg'
+import { Link } from "react-router-dom";
+
+
+const Landing =() => {
+    return (
+        <section id ="landing">
+            <header>
+                <div className="header__container">
+                    <div className="header__description">
+                        <h1>The United State's most awarded online library platform</h1>
+                        <h2>Find your dream book with <span className="purple">Library</span></h2>
+                        <Link to ="/books">
+                            <button className="btn">Browse books</button>
+                        </Link>
+                    </div>
+                    <figure className="hedaer__img--wrapper">
+                        <img src={UndrawBooks} alt="" />
+
+                    </figure>
+                </div>
+            </header>
+
+        </section>
+    )
+}
+
+export default Landing
